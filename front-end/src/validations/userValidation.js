b0013b8aa73e9d21f5f6911c807afe037aa1f143
Nomin Bat <nominbat@Nomins-MacBook-Pro.local>
2024-03-07 15:49:14 +0800

    validation

--- front-end/src/validations/userValidation.js
+++ front-end/src/validations/userValidation.js
@@ -9,8 +9,12 @@ export const SignupSchema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Required"),
   password: yup
     .string()
-    .min(4)
-    .max(10)
+    .min(4, "Password must be at least 4 charaters")
+    .max(10, "assword must be max 10 charaters")
     // .password("Invalid password")
     .required("Required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Password must match")
+    .required("Confirm password is required"),
 });
