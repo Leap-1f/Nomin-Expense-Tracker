250eb030c72890cb97ba6d80d722a3b6024ddbf3
Nomin Bat <nominbat@Nomins-MacBook-Pro.local>
2024-03-19 15:16:16 +0800

    formik validation

--- front-end/src/pages/register.jsx
+++ front-end/src/pages/register.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { SignupSchema } from "../validations/userValidation.js";
 import { useRouter } from "next/router.js";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function RegisterFunction() {
   const router = useRouter();
   // const API_ENDPOINT = "http://localhost:8080/";
-  const [userData, setUserData] = useState();
+  // const [userData, setUserData] = useState();
 
   // const handleSubmit = async (event) => {
   //   event.preventDefault();
@@ -61,12 +61,24 @@ export default function RegisterFunction() {
                   password: "",
                   confirmPassword: "",
                 }}
-                ValidationSchema={SignupSchema}
-                // onSubmit={handleSubmit}
-                onSubmit={async (values, actions) => {
-                  await new Promise((error) => setTimeout(error, 500));
-                  alert(JSON.stringify(values, null, 2));
-                  console.log(values, actions);
+                validationSchema={SignupSchema}
+                onSubmit={async (values) => {
+                  console.log(values);
+                  try {
+                    const res = await fetch("http://localhost:8080/users", {
+                      headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                      },
+                      method: "POST",
+                      body: JSON.stringify(values),
+                    });
+                    const response = await res.json();
+                    console.log(response);
+                    router.push("/steps");
+                  } catch (err) {
+                    console.log(err);
+                  }
                 }}
               >
                 <Form>
@@ -85,6 +97,7 @@ export default function RegisterFunction() {
                       // className="grow"
                       placeholder=" Enter Your Name"
                     ></Field>
+                    <ErrorMessage name="name" />
                   </label>
                   <br />
                   <label className="input input-bordered flex items-center gap-2">
@@ -103,6 +116,7 @@ export default function RegisterFunction() {
                       className="grow"
                       placeholder=" Enter Email"
                     ></Field>
+                    <ErrorMessage name="email" />
                     <br />
                   </label>
                   <br />
@@ -126,6 +140,7 @@ export default function RegisterFunction() {
                       className="grow"
                       placeholder=" Enter Password"
                     ></Field>
+                    <ErrorMessage name="password" />
                   </label>
 
                   <br />
@@ -149,120 +164,16 @@ export default function RegisterFunction() {
                       // value="password"
                       placeholder="Re-Password"
                     ></Field>
+                    <ErrorMessage name="confirmPassword" />
                   </label>
                   <br />
-                  <Link rel="stylesheet" href={{ pathname: "/loading" }}>
-                    <button type="submit" className=" btn  btn-primary ">
-                      Sign up
-                    </button>
-                  </Link>
-
-                  {/* {errors.name && <small> {errors.name}</small>} */}
-                  {/* onChange=
-                  {(event) => {
-                    setUserData({ ...userData, name: event.target.value });
-                  }} */}
-                </Form>
-              </Formik>
-              {/* <form className="flex flex-col gap-4" onSubmit={createData}>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                  </svg>
-                  <input
-                    type="text"
-                    name="name"
-                    onChange={(event) => {
-                      setUserData({ ...userData, name: event.target.value });
-                    }}
-                    className="grow"
-                    placeholder=" Enter Your Name"
-                  />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                  </svg>
-                  <input
-                    type="text"
-                    name="email"
-                    onChange={(event) =>
-                      setUserData({ ...userData, email: event.target.value })
-                    }
-                    className="grow"
-                    placeholder=" Enter Email"
-                  />
-                </label>
-
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input
-                    type="password"
-                    name="password"
-                    onChange={(event) =>
-                      setUserData({ ...userData, password: event.target.value })
-                    }
-                    className="grow"
-                    placeholder=" Enter Password"
-                    // value="password"
-                  />
-                </label>
-                <label className="input input-bordered flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input
-                    type="password"
-                    name="confirmPassword"
-                    onChange={(event) =>
-                      setUserData({
-                        ...userData,
-                        confirmPassword: event.target.value,
-                      })
-                    }
-                    className="grow"
-                    // value="password"
-                    placeholder="Re-Password"
-                  />
-                </label>
-
-                <Link rel="stylesheet" href={{ pathname: "/loading" }}>
+                  {/* <Link href={{ pathname: "/steps" }}> */}
                   <button type="submit" className=" btn  btn-primary ">
                     Sign up
                   </button>
-                </Link>
-              </form>{" "} */}
+                  {/* </Link> */}
+                </Form>
+              </Formik>
             </div>
 
             <div className="flex flex-row gap-1 *:text-base">
@@ -272,8 +183,6 @@ export default function RegisterFunction() {
               </Link>
             </div>
           </div>
-
-          {/* <div className=" bg-primary "></div> */}
         </div>
       </div>
     </div>
